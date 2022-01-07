e75d7ee612a7609a8bb824ca66653cd291265670
markitics <m@rkmoriarty.com>
2022-01-07 17:49:18 +0000

    Add /indiepreneur redirect to PowerPoint deck and /stripe-notion to the Notion doc.

--- next.config.js
+++ next.config.js
@@ -23,6 +23,22 @@ module.exports = withBundleAnalyzer({
         permanent: false
       },
       {
+        source: '/indiepreneur',
+        destination: 'https://1drv.ms/p/s!Ajsj-RKW73e1f7T7xId0-S_l1g8',
+        permanent: false
+      },
+      {
+        source: '/stripe-notion',
+        destination:
+          'https://achieved-author-f0e.notion.site/Moriarty-Stripe-Oct-2021-discussions-c3fb4e15a8ef4507b75781f5c5ae1fc2',
+        permanent: false
+      },
+      {
+        source: '/stripe-site-deck',
+        destination: 'https://1drv.ms/p/s!Ajsj-RKW73e1f7T7xId0-S_l1g8',
+        permanent: false
+      },
+      {
         source: '/tip',
         destination: 'https://buy.stripe.com/fZe14s6FOg70doIfZ0',
         permanent: false
